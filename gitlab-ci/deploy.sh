34d83e8afce3489a067b3e31713fad14ed7fe7dd
Simon Li <spli@dundee.ac.uk>
2018-04-04 12:25:36 +0100

    script is required for each job

--- gitlab-ci/deploy.sh
+++ gitlab-ci/deploy.sh
@@ -4,10 +4,14 @@ set -eu
 
 # Required environment variables:
 test -n "$CI_ENVIRONMENT_NAME"
-JUPYTERHUB_PREFIX="${CI_ENVIRONMENT_NAME#*-}"
-test -n "$JUPYTERHUB_PREFIX"
+DEPLOYMENT_NAME="${CI_ENVIRONMENT_NAME#*-}"
+test -n "$DEPLOYMENT_NAME"
 # Since we're running inside a Kubernetes pod we can use internal kube-dns
-JUPYTERHUB_URL="http://proxy-public.jupyterhub-${CI_ENVIRONMENT_NAME}/${JUPYTERHUB_PREFIX}"
+JUPYTERHUB_URL="http://proxy-public.jupyterhub-${DEPLOYMENT_NAME}/${DEPLOYMENT_NAME}"
+
+
+helmfile --hide-args --selector deployment=${DEPLOYMENT_NAME} sync --args '--wait --timeout=1200'
+
 
 # Is this necessary? Or should helm --wait take care of this?
 i=1
